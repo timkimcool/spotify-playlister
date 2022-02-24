bd68c5471c937c0986ff9123183d3683e527bea8
timkimcool <timkimcool@gmail.com>
2022-02-24 18:24:33 -0500

    update secret key, procfile

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$@&gxc_4ze5-4q+d(m_%muw#m#@0!v=9(8tc++(x%#a0bacc%n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -45,6 +44,9 @@ INSTALLED_APPS = [
 
     'spotify',
 ]
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
